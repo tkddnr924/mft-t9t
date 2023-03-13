71fd66ffa60833cc8337e6a18143749decd9ddad
tkddnr924 <tkddnr924@naver.com>
2023-03-13 22:08:08 +0900

    ✔️ make collector.go

--- collector/collector.go
+++ collector/collector.go
@@ -1,10 +1,15 @@
 package collector
 
 import (
-	// "github.com/tkddnr924/gomft/mft"
 	"github.com/tkddnr924/gomft/bootsect"
+	"github.com/tkddnr924/gomft/fragment"
+	"github.com/tkddnr924/gomft/mft"
+
+	"fmt"
 	"io"
 	"os"
+
+	"syscall"
 )
 
 const supportedOemId = "NTFS    "
@@ -17,8 +22,12 @@ type Collector struct {
 }
 
 func NewCollector(src string, dst string, letter string) *Collector {
+	volume, err := os.Open(letter)
 
-	volume, _ := os.Open(src)
+	if err != nil {
+		fmt.Printf("Unable to open volume %q: %v\n", letter, err)
+		return nil
+	}
 
 	return &Collector{
 		Target:      src,
@@ -28,24 +37,174 @@ func NewCollector(src string, dst string, letter string) *Collector {
 	}
 }
 
-func (collector *Collector) ParseBootSector() {
-	data := make([]byte, 512)
-	_, err := io.ReadFull(collector.Volume, data)
+func (collector *Collector) CollectMFT() {
+	// Parse Boot Sector
+	bootSector := ParseBootSector(collector.Volume)
+
+	if bootSector == nil {
+		fmt.Println("Unable to parse boot sector")
+		return
+	}
+
+	// Seek to $MFT
+	bytesPerCluster := bootSector.BytesPerSector * bootSector.SectorsPerCluster
+	mftPosInBytes := int64(bootSector.MftClusterNumber) * int64(bytesPerCluster)
+
+	record := collector.SeekVolume(bootSector, mftPosInBytes)
+
+	if record == nil {
+		fmt.Println("Unable to seek to $MFT")
+		return
+	}
+
+	dataRuns := collector.GetAttrData(record)
+
+	// DataRun to Fragment
+	fragments := mft.DataRunsToFragments(dataRuns, bytesPerCluster)
+	totalLength := int64(0)
+
+	for _, f := range fragments {
+		totalLength += f.Length
+	}
+
+	// Copy $MFT
+	outFile := collector.Destination + "\\$MFT"
+	out, err := os.Create(outFile)
 
 	if err != nil {
+		fmt.Printf("Unable to create %q: %v\n", outFile, err)
 		return
+	}
+	defer out.Close()
+
+	length, err := PBCopy(out, fragment.NewReader(collector.Volume, fragments))
+
+	if err != nil {
+		fmt.Printf("Unable to copy $MFT: %v\n", err)
+		return
+	}
+
+	if length != totalLength {
+		fmt.Printf("Length mismatch: %d != %d\n", length, totalLength)
+		return
+	}
+
+	info, _ := os.Stat(outFile)
+
+	fmt.Printf("%+v\n", info.Sys())
+
+	// attrs := _info.(map[string]interface{})
+
+	// fmt.Printf("%+v\n", attrs)
+
+	// if attrs, ok := _info.(map[string]interface{}); ok {
+	// 	fmt.Println(getString(attrs, "created"))
+	// } else {
+	// 	fmt.Println("not ok", ok)
+	// }
+}
+
+func getString(m map[string]interface{}, key string) string {
+	if value, ok := m[key]; ok {
+		if valueString, ok := value.(string); ok {
+			return valueString
+		}
+	}
+	return ""
+}
+
+// Get $FILE_NAME in MFT Record
+func (collector *Collector) GetAttrFileName(record *mft.Record) *mft.FileName {
+	fileNameAttribute := record.FindAttributes(mft.AttributeTypeFileName)
+	if len(fileNameAttribute) == 0 {
+		fmt.Println("No file name attributes found")
+		return nil
+	}
+
+	fileName, err := mft.ParseFileName(fileNameAttribute[0].Data)
+	if err != nil {
+		fmt.Printf("Unable to parse file name: %v\n", err)
+		return nil
+	}
+
+	return &fileName
+}
+
+// Get $DATA in MFT Record
+func (collector *Collector) GetAttrData(record *mft.Record) []mft.DataRun {
+	dataAttribute := record.FindAttributes(mft.AttributeTypeData)
+	if len(dataAttribute) == 0 {
+		fmt.Println("No data attributes found")
+		return nil
+	}
+
+	dataRuns, err := mft.ParseDataRuns(dataAttribute[0].Data)
+	if err != nil {
+		fmt.Printf("Unable to parse data runs: %v\n", err)
+		return nil
+	}
+	if len(dataRuns) == 0 {
+		fmt.Println("No data runs found")
+		return nil
+	}
+
+	return dataRuns
+}
+
+// Seek Volume
+func (collector *Collector) SeekVolume(bootSector *bootsect.BootSector, position int64) *mft.Record {
+	_, err := collector.Volume.Seek(position, 0)
+	if err != nil {
+		fmt.Printf("Unable to seek to %d: %v\n", position, err)
+		return nil
+	}
+
+	mftSizeInBytes := bootSector.FileRecordSegmentSizeInBytes
+	mftData := make([]byte, mftSizeInBytes)
+	_, err = io.ReadFull(collector.Volume, mftData)
+	if err != nil {
+		fmt.Printf("Unable to read MFT data: %v\n", err)
+		return nil
+	}
+
+	record, err := mft.ParseRecord(mftData)
+
+	if err != nil {
+		fmt.Printf("Unable to parse MFT record: %v\n", err)
+		return nil
+	}
+	return &record
+}
+
+// Copy
+func PBCopy(dst io.Writer, src io.Reader) (written int64, err error) {
+	buf := make([]byte, 1024*1024)
+	return io.CopyBuffer(dst, src, buf)
+}
+
+// Parse Boot Sector
+func ParseBootSector(volume *os.File) *bootsect.BootSector {
+	src := (io.Reader)(volume)
+
+	data := make([]byte, 512)
+	_, err := io.ReadFull(src, data)
+
+	if err != nil {
+		fmt.Printf("Unable to read boot sector data: %v\n", err)
+		return nil
 	}
 
 	bootSector, err := bootsect.Parse(data)
 
 	if err != nil {
 		fmt.Printf("Unable to parse boot sector data: %v\n", err)
+		return nil
 	}
 
 	if bootSector.OemId != supportedOemId {
 		fmt.Printf("Unknown OemId (file system type) %q (expected %q)\n", bootSector.OemId, supportedOemId)
+		return nil
 	}
-}
 
-// func (collector *Collector) Collect() {
-// }
+	return &bootSector
+}
--- main.go
+++ main.go
@@ -2,44 +2,16 @@ package main
 
 import (
 	"fmt"
-	"github.com/tkddnr924/gomft/bootsect"
-	// "github.com/tkddnr924/gomft/fragment"
-	"github.com/tkddnr924/gomft/mft"
-
 	"mft-t9t/collector"
-
-	"io"
 	"os"
 )
 
-const supportedOemId = "NTFS    "
+// func FindFileName(record mft.Record) (fileName mft.FileName) {
+// 	attrs := record.FindAttributes(mft.AttributeTypeFileName)
+// 	fileName, _ = mft.ParseFileName(attrs[0].Data)
 
-func ParseBootSector(src io.Reader) *bootsect.BootSector {
-	data := make([]byte, 512)
-	_, err := io.ReadFull(src, data)
-	if err != nil {
-		return nil
-	}
-
-	bootSector, err := bootsect.Parse(data)
-
-	if err != nil {
-		fmt.Printf("[3] Unable to parse boot sector data: %v\n", err)
-	}
-
-	if bootSector.OemId != supportedOemId {
-		fmt.Printf("[4] Unknown OemId (file system type) %q (expected %q)\n", bootSector.OemId, supportedOemId)
-	}
-
-	return &bootSector
-}
-
-func FindFileName(record mft.Record) (fileName mft.FileName) {
-	attrs := record.FindAttributes(mft.AttributeTypeFileName)
-	fileName, _ = mft.ParseFileName(attrs[0].Data)
-
-	return fileName
-}
+// 	return fileName
+// }
 
 func main() {
 	volume := "\\\\.\\C:"
@@ -50,109 +22,9 @@ func main() {
 		os.Mkdir("output", 0777)
 	}
 
-	collector := collector.NewCollector("C:\\$MFT", "output", volume)
+	pb := collector.NewCollector("C:\\$MFT", "output", volume)
 
-	fmt.Println(collector)
-
-	// fmt.Println("[*] Open Volume", volume)
-	// in, err := os.Open(volume)
-	// if err != nil {
-	// 	fmt.Println("[1]", err)
-	// }
-	// defer in.Close()
-
-	// fmt.Println("[*] Read Boot Sector")
-	// bootSector := ParseBootSector(in)
-
-	// fmt.Println("[*] Read MFT & Seek $MFT")
-	// bytesPerCluster := bootSector.BytesPerSector * bootSector.SectorsPerCluster
-	// mftPosInBytes := int64(bootSector.MftClusterNumber) * int64(bytesPerCluster)
-
-	// _, err = in.Seek(mftPosInBytes, 0)
-	// if err != nil {
-	// 	fmt.Printf("[5] Unable to seek to MFT: %v\n", err)
-	// }
-
-	// fmt.Println("[*] Parse MFT DATA")
-	// mftSizeInBytes := bootSector.FileRecordSegmentSizeInBytes // 1024 bytes
-	// mftData := make([]byte, mftSizeInBytes)
-
-	// _, err = io.ReadFull(in, mftData)
-	// if err != nil {
-	// 	fmt.Printf("[6] Unable to read MFT: %v\n", err)
-	// }
-
-	// fmt.Println("[*] Parse MFT Record")
-	// record, err := mft.ParseRecord(mftData)
-	// if err != nil {
-	// 	fmt.Printf("[7] Unable to parse MFT: %v\n", err)
-	// }
-
-	// fileName := FindFileName(record)
-
-	// fmt.Println(fileName)
-
-	// fmt.Println("[*] Parse Data Attribute")
-	// dataAttributes := record.FindAttributes(mft.AttributeTypeData)
-	// if len(dataAttributes) == 0 {
-	// 	fmt.Println("[8] No data attributes found")
-	// }
-
-	// if len(dataAttributes) > 1 {
-	// 	fmt.Println("[9] More than one data attribute found")
-	// }
-
-	// dataAttribute := dataAttributes[0]
-	// if dataAttribute.Resident {
-	// 	fmt.Println("[10] Data attribute is resident")
-	// }
-
-	// fmt.Println("[*] Parse Data Runs")
-	// dataRuns, err := mft.ParseDataRuns(dataAttribute.Data)
-
-	// if err != nil {
-	// 	fmt.Printf("[11] Unable to parse data runs: %v\n", err)
-	// }
-
-	// if len(dataRuns) == 0 {
-	// 	fmt.Println("[12] No data runs found")
-	// }
-
-	// fmt.Println("[*] Parse Data Fragments")
-	// fragments := mft.DataRunsToFragments(dataRuns, bytesPerCluster)
-	// totalLength := int64(0)
-
-	// for _, frag := range fragments {
-	// 	totalLength += int64(frag.Length)
-	// }
-
-	// outfile := "output/$MFT"
-	// out, err := openOutputFile(outfile)
-
-	// if err != nil {
-	// 	fmt.Println("[13]", err)
-	// }
-	// defer out.Close()
-
-	// fmt.Println("[*] Copy Data")
-	// n, err := CollectCopy(out, fragment.NewReader(in, fragments))
-
-	// if err != nil {
-	// 	fmt.Println("[14]", err)
-	// }
-
-	// if n != totalLength {
-	// 	fmt.Printf("[15] Expected to copy %d bytes, but copied only %d\\n", totalLength, n)
-	// }
+	pb.CollectMFT()
 
 	fmt.Println("END PROGRAM")
-}
-
-func openOutputFile(outfile string) (*os.File, error) {
-	return os.Create(outfile)
-}
-
-func CollectCopy(dst io.Writer, src io.Reader) (written int64, err error) {
-	buf := make([]byte, 1024*1024)
-	return io.CopyBuffer(dst, src, buf)
 }
